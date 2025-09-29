68aa5fce9ad8720300e60c1aa9116cfd430a1232
Ali Basoglu <alibasoglu1911@gmail.com>
2025-09-29 12:48:04 +0300

    add user entity and user model, replace auth related models

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,15 +1,16 @@
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-class UserSignUp implements UseCase<String, UserSignUpParameters> {
+class UserSignUp implements UseCase<User, UserSignUpParameters> {
   final AuthRepository authRepository;
 
   const UserSignUp({required this.authRepository});
 
   @override
-  Future<Either<Failure, String>> call(UserSignUpParameters params) async {
+  Future<Either<Failure, User>> call(UserSignUpParameters params) async {
     return await authRepository.signUpWithEmailPassword(
       name: params.name,
       email: params.email,
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,7 +23,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
       res.fold(
         (failResponse) => emit(AuthFailure(failResponse.message)),
-        (userId) => emit(AuthSuccess(userId: userId)),
+        (user) => emit(AuthSuccess(user: user)),
       );
     });
   }
